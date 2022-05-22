907b05ca6656b3c172f363ddd61c40990914a251
zzliu <tyliu0914@gmail.com>
2022-05-22 19:03:37 +0800

    v.2 第二次 Commit:圖形按鈕及捲動背景

--- app/src/main/java/liu/game/MainActivity.kt
+++ app/src/main/java/liu/game/MainActivity.kt
@@ -1,11 +1,83 @@
 package liu.game
 
+import android.graphics.Canvas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
+import kotlinx.coroutines.*
+import liu.game.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    lateinit var job: Job
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.start.isEnabled = true
+        binding.stop.isEnabled = false
+
+        binding.start.setOnClickListener(object: View.OnClickListener{
+            override fun onClick(p0: View?) {
+                job=GlobalScope.launch(Dispatchers.Main) {
+                    while(job.isActive) {
+                        binding.start.isEnabled = false
+                        binding.start.visibility = INVISIBLE
+                        binding.stop.isEnabled = true
+                        binding.stop.visibility = VISIBLE
+                        delay(25)
+                        val canvas: Canvas = binding.mysv.holder.lockCanvas()
+                        binding.mysv.drawSomething(canvas)
+                        binding.mysv.holder.unlockCanvasAndPost(canvas)
+                    }
+                }
+            }
+        })
+
+        binding.stop.setOnClickListener(object:View.OnClickListener{
+            override fun onClick(p0: View?) {
+                job.cancel()
+                binding.start.isEnabled = true
+                binding.start.visibility=VISIBLE
+                binding.stop.isEnabled = false
+                binding.stop.visibility=INVISIBLE
+            }
+        })
+
+        binding.resume.setOnClickListener(object:View.OnClickListener{
+            override fun onClick(p0: View?) {
+                job = GlobalScope.launch(Dispatchers.Main) {
+                    while(job.isActive) {
+                        binding.resume.visibility=INVISIBLE
+                        binding.stop.isEnabled = true
+                        delay(25)
+                        val canvas: Canvas = binding.mysv.holder.lockCanvas()
+                        binding.mysv.drawSomething(canvas)
+                        binding.mysv.holder.unlockCanvasAndPost(canvas)
+                    }
+                }
+            }
+        })
     }
+
+    override fun onPause() {
+        super.onPause()
+        job.cancel()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (binding.start.isEnabled == false){
+            binding.resume.visibility=VISIBLE
+            binding.stop.isEnabled = false
+        }
+    }
+
+
 }
--- app/src/main/java/liu/game/MySurfaceView.kt
+++ app/src/main/java/liu/game/MySurfaceView.kt
@@ -3,37 +3,51 @@ package liu.game
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.SurfaceHolder
 import android.view.SurfaceView
+import androidx.core.graphics.minus
 
 class MySurfaceView(context: Context?, attrs: AttributeSet?)
     : SurfaceView(context, attrs), SurfaceHolder.Callback {
     lateinit var surfaceHolder: SurfaceHolder
     lateinit var BG: Bitmap
-    //lateinit var SuperMan:Bitmap
+    var BGmoveX:Int = 0
 
     init {
         surfaceHolder = getHolder()
         BG = BitmapFactory.decodeResource(getResources(), R.drawable.background)
-        //SuperMan = BitmapFactory.decodeResource(getResources(), R.drawable.superman)
         surfaceHolder.addCallback(this)
     }
 
     override fun surfaceCreated(holder: SurfaceHolder) {
-
         var canvas: Canvas = surfaceHolder.lockCanvas()
         drawSomething(canvas)
         surfaceHolder.unlockCanvasAndPost(canvas)
     }
     fun drawSomething(canvas:Canvas) {
+        BGmoveX =BGmoveX-5
         val res = context.resources
-        var BirdX:Int = res.displayMetrics.widthPixels  //讀取螢幕寬度
-        var BirdY:Int = res.displayMetrics.heightPixels
+        var w:Int = res.displayMetrics.widthPixels  //讀取螢幕寬度
+        var h:Int = res.displayMetrics.heightPixels
         var SrcRect:Rect = Rect(0, 0, BG.width, BG.height) //裁切
-        var w:Int = BirdX
-        var h:Int = BirdY
-        var DestRect:Rect = Rect(0, 0, w, h)
-        canvas.drawBitmap(BG, SrcRect, DestRect, null)
+        var DestRect:Rect = Rect(0, 0, w, h)//顯示的區域
+        //canvas.drawBitmap(BG, SrcRect, DestRect, null)
+        var BGnewX:Int = w + BGmoveX
+
+        if (BGnewX <= 0) {
+            BGmoveX = 0
+            // only need one draw
+            canvas.drawBitmap(BG, SrcRect, DestRect, null)
+        } else {
+            // need to draw original and wrap
+            DestRect= Rect(BGmoveX,0,BGmoveX+w,h)
+            canvas.drawBitmap(BG, SrcRect, DestRect, null)
+            DestRect=Rect(BGnewX,0,BGnewX+w,h)
+            canvas.drawBitmap(BG, SrcRect, DestRect, null)
+        }
+
+
     }
 
 
